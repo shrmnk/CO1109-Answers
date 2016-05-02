efb7a66b0f3e4a584e4f8964e65b727b05fd5ff3
Sherman K <github@shrmn.com>
2016-05-02 18:55:43 +0800

    Q5d: Replaced answer with correct year
    Was previously answer to a different year's question.

--- 2015-zone-b/java-src/q5d.java
+++ 2015-zone-b/java-src/q5d.java
@@ -4,11 +4,13 @@ public class q5d {
   public static void main(String[] args) throws Exception{
     BufferedReader inone = new BufferedReader(new FileReader(args[0]));
     int t = inone.read();
+    // We are assuming that we are to perform case-sensitive comparisons
+    // Since the question literally quotes lower-case characters
     while(t != -1) {
       if(t == (int)'a')
         System.out.print('x');
-      else if(t == (int)'x')  // We are assuming that there is a typo in the paper
-        System.out.print('a');
+      else if(t == (int)'b')
+        System.out.print('y');
       else
         System.out.print((char)t);
       t = inone.read();
