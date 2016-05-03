4abbf7dcaf35f68a27648e17f1c450d0a1767131
Sherman K <github@shrmn.com>
2016-05-03 12:33:41 +0800

    Q4a: Updated mistake; add src

---
+++ 2013-zone-b/java-src/q4a.java
@@ -0,0 +1,7 @@
+class q4a {
+    public static void main(String[] args) {
+        System.out.println("pray".compareTo("prey"));
+        System.out.println("bod".compareTo("bodge"));
+        System.out.println("bodges".compareTo("bodge"));
+    }
+}
